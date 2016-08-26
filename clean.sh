946a9d4363dbb1e2c09a45e8ea0ee4e1eaca143f
subin.soman <tosubins@gmail.com>
2016-08-26 17:50:47 +0530

    added  info commit on 26 aug 16

--- clean.sh
+++ clean.sh
@@ -3,3 +3,5 @@ sudo apt-get update && sudo sudo apt-get upgrade && sudo sudo apt-get autoremove
 rm -v -f ~/.cache/thumbnails/*/*.png ~/.thumbnails/*/*.png
 rm -v -f ~/.cache/thumbnails/*/*/*.png ~/.thumbnails/*/*/*.png
 #rm -r -v ~/.mozilla && rm -r -v ~/.cache/mozilla
+echo "echo 1 > /proc/sys/vm/drop_caches" | sudo sh|free -m
+
